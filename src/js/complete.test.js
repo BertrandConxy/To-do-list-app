8c5a1bb326c79868bbdec7806d96dcb21258875e
BertrandConxy <90222110+BertrandConxy@users.noreply.github.com>
2022-01-27 14:58:59 +0200

    Create test file for our tests

---
+++ src/js/complete.test.js
@@ -0,0 +1,7 @@
+/**
+ * @jest-environment jsdom
+ */
+ import LocalStorageClass, { StoreActivity } from './add-remove-localstorage.js';
+ import CreateListItem from './create-list.js';
+ import CheckComplete from './check-complete.js';
+  
